62ed24a316673036c1b3771b988c736f8ec8eb27
Protossss <onenilz001@gmail.com>
2021-12-08 16:11:31 +0545

    commit on 8 Dec

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="logo/title_logo.png">
+    <title>College Solutions | Best for you</title>
+    <link rel="stylesheet" href="css/main_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id="main-container">
+        <header class="main-header">
+            <ul class="header-links">
+                <div class="right-links">
+                    <a href="#">College Solutions</a>
+                </div>
+                <nav class="left-links">
+                    <li><a href="#">Browse</a></li>
+                    <li><a href="#">Rankings</a></li>
+                    <li><button class="sign-in">Sign In</button></li>
+                    <li><button class="sign-up">GET STARTED</button></li>
+                </nav>
+                <a href="javascript:void(0);" id="icon" onclick="showMobileNav()">
+                    <i class="fa fa-bars"></i>
+                </a>
+
+            </ul>
+        </header>
+        <!-- <header>
+            <h1>CollegeSolutions</h1>
+
+            <nav id="menu" role="navigation">
+                <ul>
+                    <li><a href="">Browse</a></li>
+                    <li><a href="">Browse</a></li>
+                    <li><button class="sign-in">Sign In</button></li>
+                    <li><button class="sign-up">GET STARTED</button></li>
+                </ul>
+            </nav>
+
+            <div class="menu-link">
+                <div class="icon"></div>
+            </div>
+
+        </header> -->
+        <section id="banner">
+            <div class="banner-container">
+                <span>banner</span>
+            </div>
+        </section>
+        <article class="why-us">
+            <h2>Why College Solutions?</h2>
+            <p>Your search is unique.
+                Just like you.
+            </p>
+            <div class="site-features-container">
+                <div class="site-feature">
+                    <img src="#" alt="no heavy lifting">
+                    <h3>NO HEAVY LIFTING</h3>
+                    <P>We analyze data so you don't have to.</P>
+                </div>
+                <div class="site-feature">
+                    <img src="#" alt="the good, the bad & the honest">
+                    <h3>THE GOOD, THE BAD & THE HONEST</h3>
+                    <P>Our user reviews let you hear directly from families and students to give you an honest and
+                        holistic
+                        view.</P>
+                </div>
+                <div class="site-feature">
+                    <img src="#" alt="best fit">
+                    <h3>BEST FIT</h3>
+                    <P>We personalize your search based on what's most imporntant to you.</P>
+                </div>
+            </div>
+        </article>
+        <footer>
+            <div class="about-us">
+                <img src="#" alt="website logo">
+                <p>We strive to deliver a level of service that exceeds the expectations of our customers.
+
+                    If you have any questions about our products or services, please do not hesitate to contact us. We
+                    have
+                    friendly, knowledgeable representatives available seven days a week to assist you.
+                </p>
+            </div>
+            <div class="resources">
+                <ul>
+                    <li><a href="#">Blogs</a></li>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Careers</a></li>
+                </ul>
+            </div>
+            <div class="socials">
+                <ul class="social-links">
+                    <li>FaceBook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Discord</li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+
+</body>
+
+
+</html>
